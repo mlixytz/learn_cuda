cb7595b1a5e1d5110c60fe64c4c6285aaf5719a3
haoqing <haoqing.szq@alibaba-inc.com>
2024-02-04 20:19:36 +0800

    增加流函数

--- cuda/addVector.cu
+++ cuda/addVector.cu
@@ -2,35 +2,38 @@
 
 #include <cuda_runtime.h>
 
-__global__ void vectorAdd(const float* A, const float* B, float* C, int numElements) {
+__global__ void vectorAdd(const float *A, const float *B, float *C, int numElements)
+{
     int i = blockDim.x * blockIdx.x + threadIdx.x;
-    if (i < numElements) {
+    if (i < numElements)
+    {
         C[i] = A[i] + B[i];
     }
 }
 
- 
-int main() {
+int main()
+{
 
     // 元素总数
     int numElements = 50000;
     size_t size = numElements * sizeof(float);
 
     // 在host中创建A、B、C
-    float* h_A = (float*)malloc(size);
-    float* h_B = (float*)malloc(size);
-    float* h_C = (float*)malloc(size);
+    float *h_A = (float *)malloc(size);
+    float *h_B = (float *)malloc(size);
+    float *h_C = (float *)malloc(size);
 
     // 初始化 A B C
-    for (int i=0; i<numElements; ++i) {
+    for (int i = 0; i < numElements; ++i)
+    {
         h_A[i] = rand() / (float)RAND_MAX;
         h_B[i] = rand() / (float)RAND_MAX;
     }
 
     // 在GPU中给三个向量申请空间
-    float* d_A = NULL;
-    float* d_B = NULL;
-    float* d_C = NULL;
+    float *d_A = NULL;
+    float *d_B = NULL;
+    float *d_C = NULL;
     cudaMalloc((void **)&d_A, size);
     cudaMalloc((void **)&d_B, size);
     cudaMalloc((void **)&d_C, size);
@@ -46,8 +49,10 @@ int main() {
     int blockPerGrid = (numElements + threadPerBlock - 1) / threadPerBlock;
     vectorAdd<<<blockPerGrid, threadPerBlock>>>(d_A, d_B, d_C, numElements);
     cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
-    for(int i=0; i<numElements; ++i) {
-        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5) {
+    for (int i = 0; i < numElements; ++i)
+    {
+        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5)
+        {
             fprintf(stderr, "Result verfication failed at element %d!\n", i);
             exit(EXIT_FAILURE);
         }
---
+++ cuda/stream.cu
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+// (A+B)/2=C
+#define N (1024 * 1024) // 每个流执行数据大小
+#define FULL (N * 20)   // 全部数据大小
+
+__global__ void kernel(int *a, int *b, int *c)
+{
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx < N)
+    {
+        c[idx] = (a[idx] + b[idx]) / 2;
+    }
+}
+
+int main()
+{
+    // 查询设备属性
+    cudaDeviceProp prop;
+    int whichDevice;
+    cudaGetDevice(&whichDevice);
+    cudaGetDeviceProperties(&prop, whichDevice);
+    if (!prop.deviceOverlap)
+    {
+        printf("Device will not support overlap!");
+        return 0;
+    }
+    // 初始化计时器事件
+    cudaEvent_t start, stop;
+    float elaspsedTime;
+    // 声明流和Buffer指针
+    cudaStream_t stream;
+    int *host_a, *host_b, *host_c;
+    int *dev_a, *dev_b, *dev_c;
+    // 创建计时器
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    // 初始化流
+    cudaStreamCreate(&stream);
+    // 在GPU端申请存储
+    cudaMalloc((void **)&dev_a, N * sizeof(int));
+    cudaMalloc((void **)&dev_b, N * sizeof(int));
+    cudaMalloc((void **)&dev_c, N * sizeof(int));
+    // 在CPU端申请使用锁页内存
+    host_a = (int *)malloc(FULL * sizeof(int), cudaHostAllocDefault);
+    host_b = (int *)malloc(FULL * sizeof(int), cudaHostAllocDefault);
+    host_c = (int *)malloc(FULL * sizeof(int), cudaHostAllocDefault);
+    // 初始化向量A，B向量
+    for (int i = 0; i < FULL; i++)
+    {
+        host_a[i] = rand();
+        host_b[i] = rand();
+    }
+    // 开始计算
+    cudaEventRecord(start, 0);
+    for (int i = 0; i < FULL; i += N)
+    {
+        // 将数据从CPU锁页内存中传输给GPU显存
+        cudaMemcpyAsync(dev_a, host_a + i, N * sizeof(int), cudaMemcpyHostToDevice, stream);
+        cudaMemcpyAsync(dev_b, host_b + i, N * sizeof(int), cudaMemcpyHostToDevice, stream);
+        kernel<<<N / 256, 256, 0, stream>>>(dev_a, dev_b, dev_c);
+        // 将数据从GPU显存中传输给CPU内存
+        cudaMemcpyAsync(host_c + i, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost, stream);
+    }
+    cudaStreamSynchronize(stream);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elaspsedTime, start, stop);
+    printf("Time cost: %3.1f ms\n", elaspsedTime);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_c);
+    cudaFreeHost(host_a);
+    cudaFreeHost(host_b);
+    cudaFreeHost(host_c);
+    cudaStreamDestroy(stream);
+
+    return 0;
+}
